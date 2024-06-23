5cf41b64e74a4f6c02cdf4ba7016c8f7ab70a96b
Bongani Ndlovu <167146765+Ndlovu19-code@users.noreply.github.com>
2024-06-23 06:03:50 -0700

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,1 +1,14 @@
+# simple_interest.py
 
+#define variables 
+principle = 1000
+rate = 0.05
+time = 3
+
+# calculate simple interest 
+sum_result = principle + rate + time
+difference_result = principle - rate - time
+product_result = principle  * rate * time
+
+# Print the results
+print(f"The simple interest is: {interest}")
