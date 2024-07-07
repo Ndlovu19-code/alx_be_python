55f0d8a7f9784eb922b35701d79f61d734ad4a16
Bongani Ndlovu <167146765+Ndlovu19-code@users.noreply.github.com>
2024-07-07 12:04:09 -0700

    Update temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,8 +1,8 @@
 # temp_conversion_tool.py
 
 # Global conversion factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 FAHRENHEIT_TO_CELSIUS_INTERCEPT = 32
 CELSIUS_TO_FAHRENHEIT_INTERCEPT = 0  # Correction: Intercept for Celsius to Fahrenheit should be 0
 
